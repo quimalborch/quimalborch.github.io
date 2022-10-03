118bc47a1b4406c26e7bd2105adcd9db3a7be6cc
Quim Alborch López <quimalborch@gmail.com>
2022-10-03 19:50:03 +0200

    Added rfs from npm

--- about/script.js
+++ about/script.js
@@ -1,3 +1,21 @@
 function LoadedWeb() {
     setTimeout(() => {  document.getElementById("loader").style.backgroundColor = "rgba(31, 31, 31, 0)"; }, 1000);
+    
+    setTimeout(() => { GetLinkedin() }, 2000);
+}
+
+function GetLinkedin() {
+    const toastLiveExample = document.getElementById('liveToast')
+    const toast = new bootstrap.Toast(toastLiveExample)
+    toast.show()
+}
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
 }
